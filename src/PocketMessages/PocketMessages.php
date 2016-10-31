bb4cd32cd873e14c5905d6c3002c854c3f804314
Fury <xfurymcpe@gmail.com>
2016-10-30 23:03:20 -0400

    Update PocketMessages.php

--- src/PocketMessages/PocketMessages.php
+++ src/PocketMessages/PocketMessages.php
@@ -1,14 +1,21 @@
 <?php namespace PocketMessages;
 
+use PocketMessages\managers\BroadcastManager;
+
 use pocketmine\plugin\PluginBase;
 
 class PocketMessages extends PluginBase{
 
   public $broadcasts = [];
-  public $counts = [];
+  public $timers = [];
   
   public function onEnable(){
     $this->saveDefaultConfig();
+    $this->getBroadcastManager()->setup();
+  }
+  
+  public function getBroadcastManager(){
+    return new BroadcastManager($this);
   }
   
 }
